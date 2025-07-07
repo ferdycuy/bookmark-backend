89c8667bfd42c8fb4e79194a097aba361b1c93f5
ferdycuy <anaferdy01@gmail.com>
2025-07-07 20:56:41 +0700

    ok

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
 
+const app = express();
 const allowedOrigins = [
   'http://localhost:3000',
   'https://bookmark-frontend-bay.vercel.app'
@@ -20,3 +25,24 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.send('Bookmark Manager API is running');
+});
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(process.env.PORT || 5000, () =>
+      console.log(`Server running on port ${process.env.PORT || 5000}`)
+    );
+  })
+  .catch(err => console.log(err));
+
+const authRoutes = require('./routes/authRoutes');
+app.use('/api/auth', authRoutes);
+const bookmarkRoutes = require('./routes/webRoutes');
+app.use('/api/bookmarks', bookmarkRoutes);
